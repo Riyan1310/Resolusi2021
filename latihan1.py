951bc3634eaaf0ac4a8806ac38fbc43d9fa38694
RiyanRayen <59814714+Riyan1310@users.noreply.github.com>
2021-01-27 16:05:44 +0700

    Latihan Python1

---
+++ latihan1.py
@@ -0,0 +1,37 @@
+#Tipe data String
+nama = "Riyan Studio"
+
+#tipe data integer
+umur = "18"
+
+#tipe data float
+tinggi = 85
+
+#tipe data boolean
+benar = True
+salah = False
+
+#tipe data Array atau List
+nilai = [1,2,3,4,4]
+
+#tipe data objek atau dictionary
+contoh = {
+    "Key1" : "value1",
+    "key2" : "value2",
+    "key3" : "value3"
+}
+
+print(nama)
+print("===========")
+print(umur)
+print("===========")
+print(tinggi)
+print("===========")
+print(benar)
+print("===========")
+print(salah)
+print("===========")
+print(nilai[0])
+print("===========")
+print(contoh["key1"])
+print("===========")
